b8d099e8f0bbc144386603d0d2567f29a2648a2c
elite1003 <sudhirkumar10397@gmail.com>
2024-03-30 23:39:50 +0530

    added Link in aboutus page to render link

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,5 +1,21 @@
+import Link from "next/link";
+
+const details = [
+  { id: 1, name: "Yash", role: "Senior Developer" },
+  { id: 2, name: "Vaibhav", role: "Backend Developer" },
+  { id: 3, name: "Suresh", role: "Frontend Developer" },
+];
+
 const AboutUs = () => {
-  return <h1>Welcome to about us page</h1>;
+  return (
+    <ul>
+      {details.map((detail) => (
+        <li key={detail.id}>
+          <Link href={`aboutus/${detail.id}`}>{detail.name}</Link>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default AboutUs;
